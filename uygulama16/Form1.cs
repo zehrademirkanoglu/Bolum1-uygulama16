b67b2643531c9a42689d9ba98a790670db03d144
zehrademirkanoglu <145428410+zehrademirkanoglu@users.noreply.github.com>
2023-09-26 10:41:40 +0300

    Add files via upload

---
+++ uygulama16/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double turkceDogru, matDogru;
+            double turkceYanlis, matYanlis;
+            double turkceNet, matNet;
+            turkceDogru = Convert.ToDouble(textBox1.Text);
+            turkceYanlis = Convert.ToDouble(textBox2.Text);
+            turkceNet = (turkceDogru - (turkceYanlis / 4));
+            textBox3.Text = turkceNet.ToString();
+            matDogru = Convert.ToDouble(textBox4.Text);
+            matYanlis = Convert.ToDouble(textBox5.Text);
+            matNet = (matDogru - (matYanlis / 4));
+            textBox6.Text = matNet.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
